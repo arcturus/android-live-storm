27a0552edc68328e823fcc0bbf32cb2b934a8483
Francisco Jordano <arcturus@ardeenelinfierno.com>
2013-05-13 18:27:30 +0100

    Updating old dependencies

--- project.clj
+++ project.clj
@@ -6,18 +6,20 @@
   :jvm-opts ["-Djava.library.path=/usr/local/lib:/opt/local/lib:/usr/lib"]
   :repositories {
                  "twitter4j" "http://twitter4j.org/maven2",
+		 "akka" "http://repo.typesafe.com/typesafe/releases/",
 		 "local" ~(str (.toURI (java.io.File. "maven_repository")))
                  }
 
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
-                 [org.twitter4j/twitter4j-core "2.2.5-SNAPSHOT"]
-                 [org.twitter4j/twitter4j-stream "2.2.5-SNAPSHOT"]
+                 [org.twitter4j/twitter4j-core "2.2.5"]
+                 [org.twitter4j/twitter4j-stream "2.2.5"]
 		 [storm/storm-kestrel "0.6.0"]
 		 [log4j/log4j "1.2.16"]
 		 [redis.clients/jedis "2.0.0"]
                  [org.jsoup/jsoup "1.6.1"]
 		 [goose "2.1.10"]
+		 [storm "0.6.0"]
                  ]
 
   :dev-dependencies [[storm "0.6.0"]
